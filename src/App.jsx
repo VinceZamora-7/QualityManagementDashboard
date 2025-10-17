e904bd48f2845241b6405433caa90887ce2209f5
Vince Zamora (ALLYIS INC) <v-vicezamora@microsoft.com>
2025-10-17 14:50:13 +0800

    testing url

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,13 @@ function App() {
         <Header />
         <main className="flex-grow w-full">
           <Routes>
-            {/* Redirect root "/" to "/overview" */}
+            {/* Define your valid routes */}
             <Route path="/" element={<Navigate to="/overview" replace />} />
             <Route path="/overview" element={<Overview />} />
             <Route path="/email" element={<EmailDashboard />} />
             <Route path="/*" element={<EventsDashboard />} />
 
-            {/* Catch all unmatched routes and redirect to root "/" */}
+            {/* Catch-all route for 404s: pushes anyone else to "/" */}
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
